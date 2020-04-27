288748831e2c620000520b0fb357f2e202906efd
sanix-darker <s4nixd@gmail.com>
2020-04-27 20:43:09 +0100

    Aol + Dogpile fetch-ok

---
+++ app/core/Aol.py
@@ -0,0 +1,42 @@
+# Falla-Aol
+# -*- encoding: utf-8 -*-
+# Sanix-darker
+
+from app.core.Falla import Falla
+
+class Aol(Falla):
+    def __init__(self):
+
+        self.try_it = 0
+        self.max_retry = 3
+        self.source = "Aol"
+        self.mode = "requests"
+        self.results_box = "//div[@id='web']"
+        self.each_element = {
+            "tag": "li"
+        }
+        self.href = {
+            "tag": "a",
+            "type": "attribute",
+            "key": "href",
+            "child": {}
+        }
+        self.title = {
+            "tag": "a",
+            "type": "text"
+        }
+        self.cite = {
+            "tag": "div:compText",
+            "child": {
+                "tag": "p",
+                "type": "text"
+            }
+        }
+
+    def search(self, search_text):
+        
+        url = "https://search.aol.com/aol/search?q="+search_text.replace(" ", "+")
+        return self.fetch(url)
+
+a = Aol()
+print(a.search("un avion"))
---
+++ app/core/DogPile.py
@@ -0,0 +1,40 @@
+# Falla-DogPile
+# -*- encoding: utf-8 -*-
+# Sanix-darker
+
+from app.core.Falla import Falla
+
+class DogPile(Falla):
+    def __init__(self):
+
+        self.try_it = 0
+        self.max_retry = 3
+        self.source = "DogPile"
+        self.mode = "requests"
+        self.results_box = "//div[@class='mainline-results']"
+        self.each_element = {
+            "tag": "div",
+            "attr": {"class": "web-bing__result"}
+        }
+        self.href = {
+            "tag": "a:web-bing__title",
+            "type": "attribute",
+            "key": "href",
+            "child": {}
+        }
+        self.title = {
+            "tag": "a:web-bing__title",
+            "type": "text"
+        }
+        self.cite = {
+            "tag": "span:web-bing__description",
+            "type": "text"
+        }
+
+    def search(self, search_text):
+        
+        url = "https://www.dogpile.com/serp?q="+search_text.replace(" ", "+")
+        return self.fetch(url)
+
+dp = DogPile()
+print(dp.search("un avion"))
